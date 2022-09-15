d3f7714defb1d58a0a2430dffddbc3d5f0aad2d3
Rodrigo Ribeiro <jr.rodrigo.pt@gmail.com>
2022-09-15 12:42:49 +0100

    bugfix

---
+++ src/main/java/com/rce/reverseadapterdemoapp/UnversionedSchema.java
@@ -0,0 +1,9 @@
+package com.rce.reverseadapterdemoapp;
+
+public class UnversionedSchema {
+    public String p1;
+    public String p2;
+    public String p3;
+    public String p4;
+    public String p5;
+}
