6442963cbb1f41d5b44fb2b37aebddde8a098c9d
Rodrigo Ribeiro <jr.rodrigo.pt@gmail.com>
2022-09-15 14:28:54 +0100

    bugfix

--- src/main/java/com/rce/reverseadapterdemoapp/Schema.java
+++ src/main/java/com/rce/reverseadapterdemoapp/Schema.java
@@ -6,4 +6,55 @@ public class Schema {
     public String p3_v1;
     public String p4_v1;
     public String p5_v1;
+
+    public Schema() {
+    }
+
+    public Schema(String p1_v1, String p2_v1, String p3_v1, String p4_v1, String p5_v1) {
+        this.p1_v1 = p1_v1;
+        this.p2_v1 = p2_v1;
+        this.p3_v1 = p3_v1;
+        this.p4_v1 = p4_v1;
+        this.p5_v1 = p5_v1;
+    }
+
+    public String getP1_v1() {
+        return p1_v1;
+    }
+
+    public void setP1_v1(String p1_v1) {
+        this.p1_v1 = p1_v1;
+    }
+
+    public String getP2_v1() {
+        return p2_v1;
+    }
+
+    public void setP2_v1(String p2_v1) {
+        this.p2_v1 = p2_v1;
+    }
+
+    public String getP3_v1() {
+        return p3_v1;
+    }
+
+    public void setP3_v1(String p3_v1) {
+        this.p3_v1 = p3_v1;
+    }
+
+    public String getP4_v1() {
+        return p4_v1;
+    }
+
+    public void setP4_v1(String p4_v1) {
+        this.p4_v1 = p4_v1;
+    }
+
+    public String getP5_v1() {
+        return p5_v1;
+    }
+
+    public void setP5_v1(String p5_v1) {
+        this.p5_v1 = p5_v1;
+    }
 }
